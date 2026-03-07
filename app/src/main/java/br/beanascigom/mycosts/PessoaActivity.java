bfa9af1e86d519d4d83e06ba9c879ec5ce9c1153
Beatriz Gomes <beanascigom@gmail.com>
2026-03-07 18:00:18 -0300

    Refatorada no activity_pessoa

--- app/src/main/java/br/beanascigom/mycosts/PessoaActivity.java
+++ app/src/main/java/br/beanascigom/mycosts/PessoaActivity.java
@@ -14,7 +14,7 @@ public class PessoaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pessoa);
 
         editTextNome = findViewById(R.id.editTextNome);
         editTextMedia = findViewById(R.id.editTextMedia);
