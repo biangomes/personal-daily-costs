06cb7b0080cd0ff52ca98074b62c3b04cf8d6508
Beatriz Gomes <beanascigom@gmail.com>
2026-03-29 17:49:46 -0300

    Continuidade do uso de ListViews e Resources Arrays

--- app/src/main/java/br/beanascigom/mycosts/Pessoa.java
+++ app/src/main/java/br/beanascigom/mycosts/Pessoa.java
@@ -1,6 +1,16 @@
 package br.beanascigom.mycosts;
 
 public class Pessoa {
+    public Pessoa() {}
+
+    public Pessoa(String nome, int media, boolean bolsista, int tipo, MaoUsada maoUsada) {
+        this.nome = nome;
+        this.media = media;
+        this.bolsista = bolsista;
+        this.tipo = tipo;
+        this.maoUsada = maoUsada;
+    }
+
     private String nome;
     private int media;
     private boolean bolsista;
@@ -45,5 +55,16 @@ public class Pessoa {
 
     public void setMaoUsada(MaoUsada maoUsada) {
         this.maoUsada = maoUsada;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", media=" + media +
+                ", bolsista=" + bolsista +
+                ", tipo=" + tipo +
+                ", maoUsada=" + maoUsada +
+                '}';
     }
 }
--- app/src/main/java/br/beanascigom/mycosts/PessoasActivity.java
+++ app/src/main/java/br/beanascigom/mycosts/PessoasActivity.java
@@ -1,13 +1,18 @@
 package br.beanascigom.mycosts;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PessoasActivity extends AppCompatActivity {
 
     private ListView listViewPessoas;
+    private List<Pessoa> pessoas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,28 @@ public class PessoasActivity extends AppCompatActivity {
     }
 
     private void popularListaPessoas() {
+        String[] pessoas_nomes = getResources().getStringArray(R.array.pessoas_nome);
+        int[] pessoas_maos_usadas = getResources().getIntArray(R.array.pessoas_maos_usadas);
+        int[] pessoas_medias = getResources().getIntArray(R.array.pessoas_media);
+        int[] pessoas_bolsistas = getResources().getIntArray(R.array.pessoas_bolsista);
+        int[] pessoas_tipos = getResources().getIntArray(R.array.pessoas_tipo);
 
+        pessoas = new ArrayList<>();
+        Pessoa pessoa;
+        boolean bolsista;
+        MaoUsada maoUsada;
+
+        MaoUsada[] maosUsadas = MaoUsada.values();
+
+        for (int cont = 0; cont < pessoas_nomes.length; cont++) {
+            bolsista = (pessoas_bolsistas[cont] == 1 ? true : false);
+            maoUsada = maosUsadas[pessoas_maos_usadas[cont]];
+            pessoa = new Pessoa(pessoas_nomes[cont], pessoas_medias[cont], bolsista, pessoas_tipos[cont], maoUsada);
+            pessoas.add(pessoa);
+        }
+
+        ArrayAdapter<Pessoa> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pessoas);
+
+        listViewPessoas.setAdapter(adapter);
     }
 }
